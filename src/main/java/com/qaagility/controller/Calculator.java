fe4dc1e53e40bf20c64e39cde84576727866356e
GitHubSandeep1 <38283642+GitHubSandeep1@users.noreply.github.com>
2018-04-12 15:29:37 +0530

    Update Calculator.java

--- src/main/java/com/qaagility/controller/Calculator.java
+++ src/main/java/com/qaagility/controller/Calculator.java
@@ -1,9 +1,11 @@
 package com.qaagility.controller;
 
 public class Calculator {
+  int x,y, result;
 
-  public int add() {
-    return 3 + 6;
+  public int add(int x, int y) {
+    result=x+y;
+    return result;
   
   }
   
